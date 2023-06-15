c028218c80963183950e399125986ccead9da85d
Maksim <pozitiww4ik@gmail.com>
2023-06-15 14:53:29 +0400

    * new

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-import random as r
+import requests
+from config import load_config
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command, BaseFilter,Text
+from aiogram.types import Message, ContentType
+from dataclasses import dataclass
+import os
+import dotenv
+from environs import Env
 
-print(r.choice([1,2,3,4,5]))
+
+#загружаем переменные
+config = load_config('.env.txt')
+
+
+#Токен Бота
+api: str = config.tg_bot.token
+
+#обьекты бота и диспатчера
+bot: Bot = Bot(api)
+dp: Dispatcher = Dispatcher()
+
+@dp.message(Command(commands=['start']))
+async def start_cmd(message: Message):
+    await message.answer('Отправь фото')
+
+
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
